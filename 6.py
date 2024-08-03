5961d7f155191889f946a01b131c844300e6c57b
Tirumala Manav <thirumalamanav123@gmail.com>
2024-08-03 17:08:46 +0530

    Second Commit Writing a C Code to find the minimum and maximum of an array

--- 6.py
+++ 6.py
@@ -1,1 +1,34 @@
-print("About to learn Machine Learning Operations")
+// Write a C Program to find the maximum and minimum element in an array
+#include <stdio.h>
+int main()
+{
+    int n, i;
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter the elements of the array: ");
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    int max = arr[0], min = arr[0];
+    for (i = 1; i < n; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+        if (arr[i] < min)
+        {
+            min = arr[i];
+        }
+    }
+    printf("Maximum element in the array: %d\n", max);
+    printf("Minimum element in the array: %d\n", min);
+    return 0;
+}
+
+
+
+
+
